d522ebc0fba1e27762a50c4781da278be9406a0c
Thang Nguyen <nguyenhuuthanga3@gmail.com>
2022-11-15 00:13:04 +0700

    first commit

--- router/users.js
+++ router/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const routerUser = express.Router();
 
-const { register, login, logout } = require('../controller/user.controller')
+const { register, login, logout,auth} = require('../controller/user.controller')
 
 routerUser.post('/register', register)
 routerUser.post('/login', login)
